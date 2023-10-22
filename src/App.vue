3c34da89a351bfce2e1cc2910d00d8633614bc69
Khuong Tran <xuankhuong1402@gmail.com>
2023-10-22 12:29:46 +0700

    my change

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 export default {
-  setup() {
-    const router = useRouter();
-    const route = useRoute();
-
-    onBeforeMount(() => {
-      firebase.auth().onAuthStateChanged((user) => {
-        if (!user) {
-          router.replace("/login");
-        } else if (route.path == "/login" || route.path == "/register") {
-          router.replace("/");
-        }
-      });
-    });
-  },
+  setup() {},
 };
 </script>
 <style lang="scss"></style>
